f045d8e5f1c2c18d62e3bc032bd49211a51a9973
“YohanEngineer” <bouali.yohan@yahoo.com>
2022-05-23 19:29:47 +0200

    Adding summary collection

--- mongo.py
+++ mongo.py
@@ -1,11 +1,12 @@
 from encodings import utf_8
+from pydoc import doc
 from pymongo import MongoClient
 import os
 import json
 
 
 USER = os.getenv('MONGO_USER')
-PASSWORD = os.environ.get('MONGO_PASSWORD')
+PASSWORD = os.getenv('MONGO_PASSWORD')
 MONGO_URL = 'mongodb://{}:{}@192.168.1.21:27017'.format(USER,PASSWORD)
 
 client = MongoClient(MONGO_URL)
@@ -24,11 +25,13 @@ surah_name = json.load(chapters)
 
 #Getting each surah transliteration
 for surah in surah_name:
-    list_of_surah_name.append(surah['transliteration'])
+    list_of_surah_name.append((surah['transliteration'], surah['id']))
 
 
-for name in list_of_surah_name:
-    db.create_collection(name)
+for surah in list_of_surah_name:
+    db.create_collection(surah[0])
+    document = {'number' : surah[1], 'translation': surah[0]}
+    db.summary.insert_one(document)
 
 #Retrieve Quran chapters with translation 
 data = open('resources/quran_fr.json','r',encoding='utf-8')
@@ -37,7 +40,7 @@ data = open('resources/quran_fr.json','r',encoding='utf-8')
 surah_data = json.load(data)
 
 for surah in surah_data:
-    if surah['transliteration'] in list_of_surah_name:
+    if surah['transliteration'] in [name[0] for name in list_of_surah_name]:
         collection = surah['transliteration']
         print(collection)
         verses = surah['verses']
