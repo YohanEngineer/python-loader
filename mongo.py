8e338f66a7ac92bad7e450869439f9947b9a038b
“YohanEngineer” <yohan.bouali@etu.u-pec.fr>
2022-11-16 17:53:36 +0100

    Setup mongo atlas connection

--- mongo.py
+++ mongo.py
@@ -1,54 +1,72 @@
 from encodings import utf_8
 from pydoc import doc
-from pymongo import MongoClient
+import pymongo
 import os
 import json
 
 
 USER = os.getenv('MONGO_USER')
 PASSWORD = os.getenv('MONGO_PASSWORD')
-MONGO_URL = 'mongodb://{}:{}@192.168.1.21:27017'.format(USER,PASSWORD)
 
-client = MongoClient(MONGO_URL)
-client.drop_database('quran')
-client.drop_database('quran_fr')
+# MONGO_URL = 'mongodb://{}:{}@192.168.1.21:27017'.format(USER,PASSWORD)
+MONGO_URL = 'mongodb+srv://{}:{}@cluster0.limnd9j.mongodb.net/?retryWrites=true&w=majority'.format(USER,PASSWORD)
+
+
+try:
+    client = pymongo.MongoClient(MONGO_URL,
+        connectTimeoutMS=30000,
+        socketTimeoutMS=None)
+    print("Connection successful")
+except:
+    print("Unsuccessful connection")
+
+
+
+# client.drop_database('quran')
+# client.drop_database('quran_fr')
 db=client.quran_fr
 
 
-list_of_surah_name = []
-
-#Retrieve Quran chapters 
-chapters = open('resources/index.json','r',encoding='utf-8')
-
-#Loading the json inside the surah_name variable
-surah_name = json.load(chapters)
-
-#Getting each surah transliteration
-for surah in surah_name:
-    list_of_surah_name.append((surah['transliteration'], surah['id']))
+def ingestion_process():
 
 
-for surah in list_of_surah_name:
-    db.create_collection(surah[0])
-    document = {'number' : surah[1], 'translation': surah[0]}
-    db.summary.insert_one(document)
+    list_of_surah_name = []
 
-#Retrieve Quran chapters with translation 
-data = open('resources/quran_fr.json','r',encoding='utf-8')
+    #Retrieve Quran chapters 
+    chapters = open('F:/Documents/Documents/VS-projects/python-etl/resources/index.json','r',encoding='utf-8')
+
+    #Loading the json inside the surah_name variable
+    surah_name = json.load(chapters)
+
+    #Getting each surah transliteration
+    for surah in surah_name:
+        list_of_surah_name.append((surah['transliteration'], surah['id']))
 
 
-surah_data = json.load(data)
+    for surah in list_of_surah_name:
+        print('Creating collection {}'.format(surah[0]))
+        db.create_collection(surah[0])
+        document = {'number' : surah[1], 'translation': surah[0]}
+        db.summary.insert_one(document)
 
-for surah in surah_data:
-    if surah['transliteration'] in [name[0] for name in list_of_surah_name]:
-        collection = surah['transliteration']
-        print(collection)
-        verses = surah['verses']
-        documents = []
-        for verse in verses:
-            aya_number = verse['id']
-            translation = verse['translation']
-            document = {'aya_number' : aya_number, 'translation': translation}
-            documents.append(document)
-        db.get_collection(collection).insert_many(documents)
+    #Retrieve Quran chapters with translation 
+    data = open('F:/Documents/Documents/VS-projects/python-etl/resources/quran_fr.json','r',encoding='utf-8')
 
+
+    surah_data = json.load(data)
+
+    for surah in surah_data:
+        if surah['transliteration'] in [name[0] for name in list_of_surah_name]:
+            collection = surah['transliteration']
+            print('Inserting in collection {}'.format(surah['transliteration']))
+            verses = surah['verses']
+            documents = []
+            for verse in verses:
+                aya_number = verse['id']
+                translation = verse['translation']
+                document = {'aya_number' : aya_number, 'translation': translation}
+                documents.append(document)
+            db.get_collection(collection).insert_many(documents)
+
+
+ingestion_process()
